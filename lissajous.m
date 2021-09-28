7cd90f1f477f703e9a15dc9a5efa46c74fc8c8f2
Simon Alexander Reding <simon.reding@epfl.ch>
2021-09-28 06:49:54 +0000

    Update lissajous.m

--- lissajous.m
+++ lissajous.m
@@ -1,7 +1,7 @@
 %% Parameter Definition
 % Number of points per trajectory:
 clear all
-m = 500;
+m = 5000;
 
 % max/min angles:
 min_roll = -20;
@@ -11,22 +11,23 @@ max_pitch = 15;
 min_yaw = -10;
 max_yaw = 10;
 
-rng(4281529)
+rng(4281529);
 
 [roll, pitch, yaw] = generate_angles(m, min_roll, max_roll, min_pitch, max_pitch, min_yaw, max_yaw);
 
+scatter3(roll, pitch, yaw);
 
 %% Functions
 function [roll, pitch, yaw] = generate_angles(m, min_roll, max_roll, min_pitch, max_pitch, min_yaw, max_yaw)
-    sample_arrays = lhsdesign(5,m);
-    t = linspace(-pi, pi, m);
+    sample_arrays = lhsdesign(1, 5)*pi*4;
+    t = linspace(-pi, pi, m)';
 
-    a = sample_arrays(1,:);
-    b = sample_arrays(2,:);
-    c = sample_arrays(3,:);
-
-    delta = pi./sample_arrays(4,:);
-    phi = pi./sample_arrays(5,:);
+    a = sample_arrays(:,1);
+    b = sample_arrays(:,2);
+    c = sample_arrays(:,3);
+    
+    delta = pi./sample_arrays(:,4);
+    phi = pi./sample_arrays(:,5);
     
     roll = standardize_to_vals(sin(a.*t + delta), min_roll, max_roll);
     pitch = standardize_to_vals(sin(b.*t), min_pitch, max_pitch);
@@ -35,8 +36,7 @@ end
 
 function x = standardize_to_vals(x, min_val, max_val)
     x = x - min(x);
-    x = x * (1/max(x));
+    x = x /max(x);
     x = x * (max_val - min_val);
     x = x + min_val;
-    
-end 
+end
